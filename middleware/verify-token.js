ab3eec79333827f96e79b6522fa97440f4b70ab4
theamazingmrb <billie.heidelberg.jr@gmail.com>
2026-02-05 09:38:38 -0800

    fixed verifyToken middleware split function

--- middleware/verify-token.js
+++ middleware/verify-token.js
@@ -2,16 +2,22 @@ const jwt = require("jsonwebtoken");
 
 function verifyToken(req, res, next) {
   try {
-    const token = req.headers.authorization.split(" ")[1];
+    const token = req.headers.authorization?.split(" ")[1];
+
+    // Make sure we have a token
+    if (!token) {
+      throw new Error("You must be authorized to access this content");
+    }
+
     // decoded will return a error or the payload from the token
     const decoded = jwt.verify(token, process.env.SECRET);
     // add users info (username, _id) onto the request so we can use it
     // in tings like Task.find({owner: req.user._id})
-    req.user = decoded.payload
+    req.user = decoded.payload;
 
-    next()
+    next();
   } catch (error) {
-    res.status(401).json({err: error.message})
+    res.status(401).json({ err: error.message });
   }
 }
 
